f4f75195de5d5f43d5b4f8f61f7ac1c1a3e10d83
dangnc <dangnc.cntt@gmail.com>
2021-11-15 02:00:58 +0700

    fix

---
+++ src/modules/productDetail/ProductDetail.tsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {observer} from "mobx-react";
+
+@observer
+class ProductDetailStore extends Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ProductDetailStore;
